44e25d7f5f0c3fef1c28250feb47dcd1fa926fc7
Ragupathi-D <48428043+Ragupathi-D@users.noreply.github.com>
2021-05-25 11:34:15 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app id="app"> </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menu: [
+        { icon: "home", title: "Link A" },
+        { icon: "info", title: "Link B" },
+        { icon: "warning", title: "Link C" },
+      ],
+    };
+  },
+
+  methods: {
+    menuItems() {
+      return this.menu;
+    },
+  },
+};
+</script>
